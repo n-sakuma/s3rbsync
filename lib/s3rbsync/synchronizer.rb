6d77fa0cd027e3803f9a58fd4a81a3a9429ceb44
Naohiro Sakuma <nao.bear@gmail.com>
2013-09-03 02:44:21 +0900

    Implement sync logic. (TODO: directory)

--- lib/s3rbsync/synchronizer.rb
+++ lib/s3rbsync/synchronizer.rb
@@ -23,10 +23,48 @@ module S3rbsync
     end
 
     def sync!
-      # ...
-      # puts "sync_dir", @sync_dir
-      # puts "cofig---"
-      # p @configure
+      # puts '...call sync!'
+      upload_directory(@local_dir)
     end
+
+
+    private
+
+    def upload_directory(dir)
+      Dir.entries(@local_dir).each do |file|
+        next if file.start_with?('.')
+
+        if File.directory?(file)
+          # TODO
+        else
+          upload_file(file)
+        end
+      end
+    end
+
+    def upload_file(file_name)
+      remote_file = get_remote_file(file_name)
+      return if remote_file && (not modified?(file_name, remote_file))
+      destroy_file(remote_file) if remote_file && modified?(file_name, remote_file)
+      @bucket.files.create(:key => "#{file_name}", :body => open(file_name), :public => false)
+      puts "copied #{file_name}"
+    end
+
+    def get_remote_file(file_name)
+      @bucket.files.get(file_name)
+    end
+
+    def modified?(local_file, remote_file)
+      # puts "verifying file: #{local_file}"
+      return true unless remote_file
+      File.mtime(local_file) > remote_file.last_modified
+    end
+
+    def destroy_file remote_file
+      return unless remote_file
+      remote_file.destroy
+      # puts "delete on s3 #{remote_file.key}"
+    end
+
   end
 end
